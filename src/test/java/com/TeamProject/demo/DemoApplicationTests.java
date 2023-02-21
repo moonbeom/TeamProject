d6f0f2ac6554a855053a86e6b3f3197020414fc7
moonbeom <tjansqja789@naver.com>
2023-02-21 09:36:34 +0900

    Initial commit

---
+++ src/test/java/com/TeamProject/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TeamProject.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
